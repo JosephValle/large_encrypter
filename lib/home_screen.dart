4b91312b99a0ec4bc6d75ceaba0f2869f7ca0bb7
JosephValle <josephv2599@gmail.com>
2023-06-13 11:30:15 -0400

    Init

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -76,44 +76,6 @@ class _HomeScreenState extends State<HomeScreen> {
         print(file!.size);
         print(file!.name);
 
-        // Open http client
-        // Generate a Key
-        final Uint8List key = EncryptApi().generateAesKey();
-        // Encrypt the Key
-
-        // Init the Upload
-
-        const chunkSize = 1024 * 1024 * 50; // 50MB chunk size
-        final totalChunks = (file!.size / chunkSize).ceil();
-
-        for (var i = 0; i < totalChunks; i++) {
-          final start = i * chunkSize;
-          final end = (start + chunkSize >= file!.size)
-              ? file!.size
-              : start + chunkSize;
-
-          final chunk = file!.slice(start, end);
-          final reader = FileReader();
-
-          reader.onLoadEnd.listen((event) {
-            if (reader.readyState == FileReader.DONE) {
-              final chunkData = reader.result;
-
-
-              if (i == totalChunks - 1) {
-
-              }
-            }
-          });
-
-          reader.readAsArrayBuffer(chunk);
-        }
-        // Get a Chunk
-        // Encrypt that Chunk
-        // Upload that chunk
-
-        // End For Loop
-        // Close Client?
       }
     });
 
@@ -121,47 +83,4 @@ class _HomeScreenState extends State<HomeScreen> {
     input.click();
   }
 
-  Uint8List encryptChunk(Uint8List chunk, Uint8List key) {
-    throw UnimplementedError();
-  }
-//
-// void encryptFileInChunks(File file) {
-//   final reader = FileReader();
-//   final chunkSize = 1024; // Adjust the chunk size as per your requirement
-//
-//   reader.onLoad.listen((e) {
-//     final fileContent = reader.result as List<int>;
-//     final encryptedContent = encryptData(fileContent);
-//     // Do something with the encrypted content, such as sending it to a server
-//   });
-//
-//   int offset = 0;
-//   void readChunk() {
-//     final blob = file.slice(offset, offset + chunkSize);
-//     reader.readAsArrayBuffer(blob);
-//     offset += chunkSize;
-//
-//     if (offset < file.size) {
-//       window.requestAnimationFrame((_) => readChunk());
-//     }
-//   }
-//
-//   readChunk();
-// }
-//
-// List<int> encryptData(List<int> data) {
-//   // Perform encryption using the crypto library
-//   // Example: using AES encryption
-//   final key = generateEncryptionKey(); // Generate your encryption key
-//   // final cipher = AES(key);
-//   final encryptedData = cipher.encrypt(data);
-//   return encryptedData;
-// }
-//
-// List<int> generateEncryptionKey() {
-//   final random = Random.secure();
-//   const keyLength = 32;
-//   return List<int>.generate(keyLength, (_) => random.nextInt(256));
-// }
-//
 }
